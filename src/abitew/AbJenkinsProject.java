b3949cffff6b71fb56756790e9605ce8a20be45c
abaynheabitew <abaynheabitew@yahoo.com>
2018-03-02 17:25:28 -0500

    here we go again

--- src/abitew/AbJenkinsProject.java
+++ src/abitew/AbJenkinsProject.java
@@ -3,11 +3,7 @@ import java.util.*;
 public class AbJenkinsProject {
 		public static void main(String[] args) {
 			// TODO Auto-generated method stub
-			Scanner keyboard = new Scanner(System.in);
-			String name;
-			System.out.println("Your name ");
-			name= keyboard.nextLine();
-			System.out.println(name);
+		System.out.println("here we go again!!");
 
 		}
 

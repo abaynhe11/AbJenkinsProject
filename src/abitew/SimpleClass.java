a21a699c91e20e713b9154a63e413f37fa60a94c
abaynheabitew <abaynheabitew@yahoo.com>
2018-03-02 17:02:58 -0500

    here is the first sample project on jenkins

---
+++ src/abitew/SimpleClass.java
@@ -0,0 +1,15 @@
+package abitew;
+import java.util.Scanner;
+public class SimpleClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner keyboard = new Scanner(System.in);
+		String name;
+		System.out.println("Your name ");
+		name= keyboard.nextLine();
+		System.out.println(name);
+
+	}
+
+}

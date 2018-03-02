574f7998e8988303783c630d6c07ec1b0667a5f2
abaynheabitew <abaynheabitew@yahoo.com>
2018-03-02 17:19:43 -0500

    here we go again

---
+++ src/abitew/AbJenkinsProject.java
@@ -0,0 +1,14 @@
+package abitew;
+import java.util.*;
+public class AbJenkinsProject {
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Scanner keyboard = new Scanner(System.in);
+			String name;
+			System.out.println("Your name ");
+			name= keyboard.nextLine();
+			System.out.println(name);
+
+		}
+
+	}
